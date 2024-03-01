88feb17e83c01949098b391746ec2bfc44f8ef2f
GeraldNewton <arpitnewton1@gmail.com>
2024-03-01 21:41:53 +0530

    first

--- xweatherapp/src/App.js
+++ xweatherapp/src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [city, setCity] = useState({});
+  const [display, setDisplay] = useState({ data: false, load: false });
+  let search = async (e) => {
+    e.preventDefault();
+    setDisplay({ data: false, load: true });
+    const { text } = e.target.elements;
+    console.log(text.value);
+    try {
+      let res = await axios.get("https://api.weatherapi.com/v1/current.json", {
+        params: {
+          Key: "cf2f2a6f9ffb42e594f75440232909",
+          q: `${text.value}`,
+        },
+      });
+      // if (res.data.error) {
+      //   setDisplay({ data: false, load: false });
+      //   alert(res.data.error);
+      // } else {
+      setCity(res.data);
+      setDisplay({ data: true, load: false });
+      // }
+    } catch (e) {
+      setDisplay({ data: false, load: false });
+      alert("Failed to fetch weather data");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={search} className="form">
+        <input type="text" name="text" className="input" />
+        <button type="submit" className="btn">
+          Search
+        </button>
+      </form>
+      {display.data && (
+        <div className="data">
+          <div className="weather-card">
+            <div className="heading">Temperature</div>
+            <div className="inner-data">{city.current.temp_c}°C</div>
+          </div>
+          <div className="weather-card">
+            <div className="heading">Humidity</div>
+            <div className="inner-data">{city.current.humidity}%</div>
+          </div>
+          <div className="weather-card">
+            <div className="heading">Condition</div>
+            <div className="inner-data">{city.current.condition.text}</div>
+          </div>
+          <div className="weather-card">
+            <div className="heading">Wind Speed</div>
+            <div className="inner-data">{city.current.wind_kph}kph</div>
+          </div>
+        </div>
+      )}
+      {display.load && <p className="load">Loading data...</p>}
+    </>
   );
 }
 
